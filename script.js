652ae6840eb7fb2c0ab65ddc803b5418aa3ace25
ClayWaits <114945274+ClayWaits@users.noreply.github.com>
2023-07-02 18:27:49 -0400

    Add a little HTML and todo list in JS file

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ function Book() {
 
 }
 
-function addBookTo
+function addBookToLibrary() {
+    
+}
+
+//TODO:
+//Add input in HTML to add a book, pages, author, read/unread
+//Add placeholder books
+//Add responsive cards
+//Make it look pretty
+//Add function to add books
+//Define book function
+//Make it functional
