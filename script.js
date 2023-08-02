9e806a6113c4e93f8015486431dae2ccb0c31d4f
ClayWaits <114945274+ClayWaits@users.noreply.github.com>
2023-08-02 11:56:33 -0400

    add event listener for submit button, add logic for inputting book from form

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+const addBookForm = document.getElementById("addBookForm")
+let bookTitle = document.getElementById("bookTitleInput");
+let author = document.getElementById("authorInput");
+let pages = document.getElementById("pageInput");
+let read = document.getElementById("readCheck")
+
+addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newBook = getBookFromInput()
+    if (bookTitle.value == "" || author.value == "" || pages.value == "" || read.value == "") {
+        alert("Please fill out the entire form before submitting your book!")
+    } else if (isNaN(pages.value)) {
+        alert("Number of pages must be a number value")
+    } else {
+        console.log(`Submitted! This form has a title of ${bookTitle.value} author is ${author.value} page number is ${pages.value} and the read value is ${read.checked}`)
+    }
+});
+
+function getBookFromInput() {
+    const title = document.getElementById('bookTitleInput').value
+    const author = document.getElementById('authorInput').value
+    const pages = document.getElementById('pagesInput').value
+    const isRead = document.getElementById("readCheck").checked
+    return new Book(title, author, pages, isRead)
+}
+
+function toggleRead() {
+
+}
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+    addBookForm.reset();
+  }
+
+const library = new Library()
+
 let myLibrary = [];
 
 function Book() {
@@ -8,11 +45,14 @@ function addBookToLibrary() {
     
 }
 
+
+//Button selectors
 //TODO:
-//Add input in HTML to add a book, pages, author, read/unread
+
 //Add placeholder books
 //Add responsive cards
 //Make it look pretty
 //Add function to add books
 //Define book function
 //Make it functional
+
